b39a1e3f1e59fb1c025e110ee9b3620e5cb29d2a
Keerthana S Iyer <kiyer@griddynamics.com>
2025-03-17 11:46:16 +0530

    Delete SQL directory

--- SQL/exercise_11.sql
+++
@@ -1,7 +0,0 @@
--- Swap Salary --
-
-UPDATE Salary
-SET sex = CASE 
-             WHEN sex = 'm' THEN 'f'
-             WHEN sex = 'f' THEN 'm'
-          END;
--- SQL/exercise_6.sql
+++
@@ -1,6 +0,0 @@
--- Employees Earning More Than Their Managers --
-
-SELECT e.name AS Employee
-FROM Employee e
-JOIN Employee m ON e.managerId = m.id
-WHERE e.salary > m.salary;
